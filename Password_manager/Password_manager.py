222af26676aa731a35e5f79b35c128c2badbc2de
VirajDalvi79 <virajdalvi209@gmail.com>
2026-06-15 20:22:03 +0530

    Password manager v3

--- Password_manager/Password_manager.py
+++ Password_manager/Password_manager.py
@@ -1,4 +1,5 @@
 passwords = {}
+master_code = "codewithshadow"
 try:
    with open("passwords.txt", "r") as file:
     for line in file:
@@ -13,7 +14,8 @@ while True:
    print("""=== Password Manager ===
       1. Store a password
       2. View stored passwords
-      3. Exit""")
+      3. delete a password   
+      4. Exit""")
    option = int(input("Choose an option: "))
    
 
@@ -22,19 +24,44 @@ while True:
     code = input("Enter a password to store: ")
     passwords[where] = code
     with open("passwords.txt", "w") as file:
-       print(file.name)
+       print("Password saved successfully.")
        for where, code in passwords.items():
           file.write(f"{where} : {code}\n")
     
     
    elif option == 2:
     view = str(input("which password do you want to view?: "))
+    master = input("Enter the master code to view the password: ")
+    if master == master_code:
+       pass
+    else:
+         print("Incorrect master code. Cannot view the password.")
+         continue
+    
     if view in passwords:
-      print(passwords[view])
+      print(f"The password for {view} is : {passwords[view]}")
     else:
         print("Password not found.")
 
    elif option == 3:
+        delete_website = input("Which password do you want to delete?: e.g., 'email', 'bank', etc.: ")
+        master = input("Enter the master code to delete the password: ")
+        if master == master_code:
+           pass 
+        else:
+             print("Incorrect master code. Cannot delete the password.")
+             continue
+
+        if delete_website in passwords:
+           del passwords[delete_website]
+           print(f"Password for {delete_website} deleted successfully.")
+           with open("passwords.txt", "w") as file:
+              for where, code in passwords.items():
+                 print(file.name)
+                 file.write(f"{where} : {code}\n")
+                 
+
+   elif option == 4:              
         print("Exiting the program. Goodbye!")
         break
    
